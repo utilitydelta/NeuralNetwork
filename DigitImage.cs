2a644f5d33671a39d15977406e4aa2f5196b8836
Tyson Brown <tysonbrown@live.com.au>
2020-12-29 09:47:11 +1000

    first commit

---
+++ DigitImage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuralNetwork
+{
+    public class DigitImage
+    {
+        public byte[,] pixels;
+        public byte label;
+
+        public DigitImage(byte[,] pixels,
+          byte label)
+        {
+            this.pixels = new byte[28,28];
+
+            for (int i = 0; i < 28; ++i)
+                for (int j = 0; j < 28; ++j)
+                    this.pixels[i,j] = pixels[i,j];
+
+            this.label = label;
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+            for (int i = 0; i < 28; ++i)
+            {
+                for (int j = 0; j < 28; ++j)
+                {
+                    if (this.pixels[i,j] == 0)
+                        s += " "; // white
+                    else if (this.pixels[i,j] == 255)
+                        s += "O"; // black
+                    else
+                        s += "."; // gray
+                }
+                s += "\n";
+            }
+            s += this.label.ToString();
+            return s;
+        } // ToString
+
+    }
+}
